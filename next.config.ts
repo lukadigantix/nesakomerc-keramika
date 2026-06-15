b51624f3f31f5b4c216ccb5df228b2bb3ff810eb
lukadigantix <luka.n@digantix.com>
2026-06-15 11:55:15 +0200

    config files

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "api.nesakomerckeramika.com",
-        pathname: "/uploads/**",
+        pathname: "/**",
       },
     ],
   },
